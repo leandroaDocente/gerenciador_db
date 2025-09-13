9e7e7aedec0e89acfd00b6468975fa769c5a4a64
leandroaDocente <leandro.a@docente.senai.br>
2025-09-12 21:36:54 -0300

    script.sql adicionado

---
+++ script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS tarefa_db;
+USE tarefa_db;
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    completed BOOLEAN DEFAULT FALSE
+);
+
+SELECT * FROM tasks;
